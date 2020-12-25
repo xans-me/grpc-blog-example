fc00c1b545b242ac569943432ab730ee681be448
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2020-12-26 04:10:53 +0700

    Add CreateBlog Client

--- client/main.go
+++ client/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
+	"github.com/xans-me/grpc-blog-example/protobuff"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	fmt.Println("Hello i am client")
+	fmt.Println("Blog Client")
 
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
@@ -16,4 +18,16 @@ func main() {
 	}
 	defer conn.Close()
 
+	c := protobuff.NewBlogServiceClient(conn)
+
+	blog := &protobuff.Blog{
+		AuthorId: "xans",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog",
+	}
+	resp, err := c.CreateBlog(context.Background(), &protobuff.CreateBlogRequest{Blog: blog})
+	if err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+	fmt.Printf("Blog has been created successfully : %v", resp)
 }
